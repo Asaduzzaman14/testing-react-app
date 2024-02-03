e22d7bf8c0567c25d03adbcfd8400fc81941da06
Md Asaduzaman <asadasad9282@gmail.com>
2024-02-03 20:31:07 +0600

    FindBy() Method

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,59 @@
 import './App.css';
 import PrimaryButton from './components/PrimaryButton';
+import UserTable from './components/UsersTable/UsersTable';
 import FruitList from './components/fullList/fruitList';
 import User from './components/user/User';
-AnimationEffect;
+
+
+
+export const users = [
+   {
+      id: 1,
+      name: 'user 1',
+      age: 22
+   },
+   {
+      id: 2,
+      name: 'user 1',
+      age: 44
+   },
+   {
+      id: 3,
+      name: 'user 1',
+      age: 11
+   }
+];
+
 function App() {
 
-   const fruits = ['apple', 'banana', 'orange'];
+   // const fruits = ['apple', 'banana', 'orange'];
+   const users = [
+      {
+         id: 1,
+         name: 'user 1',
+         age: 22
+      },
+      {
+         id: 2,
+         name: 'user 1',
+         age: 44
+      },
+      {
+         id: 3,
+         name: 'user 1',
+         age: 11
+      }
+   ];
    return (
       <div>
          <h1>Hello World</h1>
          {/* <PrimaryButton ></PrimaryButton>
          <PrimaryButton actionType="add"></PrimaryButton> */}
 
-         <User />
-         <FruitList fruits={fruits} />
+         {/* <User /> */}
+         {/* <FruitList fruits={fruits} /> */}
+
+         <UserTable users={users} />
       </div>
    );
 }
---
+++ src/components/UsersTable/UserTable.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import UserTable from "./UsersTable";
+import { users } from "../../App";
+
+
+
+
+
+describe('userTable', () => {
+  it('should render the welcome message after 500 ms', async () => {
+    render(<UserTable users={users} />);
+
+    const welcomeMessage = await screen.findByTestId('welcome', {}, { timeout: 2000 });
+
+
+    expect(welcomeMessage).toBeInTheDocument();
+  });
+});
